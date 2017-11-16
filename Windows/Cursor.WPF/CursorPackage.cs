b71ecea99f4f1764d345ddd132fa29717a6a5e1f
Kevin VanGelder <kevin@infinite.red>
2017-11-16 14:56:43 -0800

    initial WPF cursor implementation

---
+++ Windows/Cursor.WPF/CursorPackage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace Cursor
+{
+    public class CursorPackage : IReactPackage
+    {
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>
+            {
+                new CursorModule()
+            };
+        }
+
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
+        {
+            return new List<Type>(0);
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers(ReactContext reactContext)
+        {
+            return new List<IViewManager>(0);
+        }
+    }
+}
